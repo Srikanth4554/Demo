b5737ed42b6f296efb31908b982e772096916d16
Srikanth <srikanth@example.com>
2024-12-16 12:27:42 +0530

    intial commit

---
+++ src/main/java/com/hellosecurity/springJWT/dto/User.java
@@ -0,0 +1,30 @@
+package com.hellosecurity.springJWT.dto;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails{
+	
+	private int id;
+	private String firstname;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
